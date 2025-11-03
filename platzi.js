6d4dff24c28f7a81dfa77312300725fd0b471617
Jeyz29 <gbernev@gmail.com>
2025-11-03 12:09:04 -0400

    V3 movimiento del cerdo

--- platzi.js
+++ platzi.js
@@ -4,7 +4,6 @@ var teclas = {
   LEFT: 37,
   RIGHT: 39
 };
-console.log(teclas);
 
 
 
@@ -41,24 +40,20 @@ cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener("load", cargarCerdo);
 
 
-function cargarFondo()
-{
+function cargarFondo(){
   fondo.cargaOK = true;
-  dibujar();
+  dibujar(200,200);
 };
-function cargarVacas()
-{
+function cargarVacas(){
   vaca.cargaOK = true;
-  dibujar();
+  dibujar(200,200);
 };
-function cargarCerdo()
-{
+function cargarCerdo(){
   cerdo.cargaOK = true;
-  dibujar()
+  dibujar(200,200)
 }
 
-function dibujar()
-{
+function dibujar(posx,posy){
   if(fondo.cargaOK)
   {
     papel.drawImage(fondo.imagen, 0, 0);
@@ -78,47 +73,46 @@ function dibujar()
   if(cerdo.cargaOK)
   {
     console.log("cargarCerdo")
-    var x = 200
-    var y = 200
-    papel.drawImage(cerdo.imagen, x, y);
+    papel.drawImage(cerdo.imagen, posx, posy);
 
   }
 }
 
-function moverCerdo(evento)
-{
   var movimiento = 10;
   var x = 200;
   var y = 200;
-  switch (evento.keyCode)
-  {
+
+function moverCerdo(evento){
+  switch (evento.keyCode){
     case teclas.UP:
     moverCerdo(cerdo - movimiento, papel);
     y = y - movimiento;
      console.log("arriba");
+     dibujar(x,y);
      break;
     case teclas.DOWN:
     moverCerdo(cerdo.imagen + movimiento, papel);
     y = y + movimiento;
      console.log("abajo");
+     dibujar(x,y);
      break;
     case teclas.LEFT:
     moverCerdo(cerdo.imagen - movimiento, y, papel);
     x = x - movimiento;
      console.log("izquierda");
+     dibujar(x,y);
      break;
     case teclas.RIGHT:
     moverCerdo(cerdo.imagen + movimiento, y, papel);
     x = x + movimiento;
       console.log("derecha");
+      dibujar(x,y);
     break;
-    default:
-    console.log("Otra tecla");
   }
 }
 
-function aleatorio(min, maxi)
-{
+
+function aleatorio(min, maxi){
   var resultado;
   resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
   return resultado;
